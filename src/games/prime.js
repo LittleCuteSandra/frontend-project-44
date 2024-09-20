39c69c249f6681d79e7d61d73e1aebee9a9cfed7
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-09-20 17:40:51 +0300

    add new game Prime. src/games/prime.js - file with the game code, bin/brain-prime.js - executable file

---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import game from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+    if (number < 2) {
+        return false;
+    }
+    let divider = 2;
+    while (divider <= number/2) {
+        if (number % divider === 0) {
+            return false;
+        }
+        divider += 1;
+    }
+    return true;
+};
+
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const getQuestionAndAnswer = () => {
+    const question = randomNumber();
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+    return [question, correctAnswer];
+};
+
+export default () => {
+    game(description, getQuestionAndAnswer);
+};
