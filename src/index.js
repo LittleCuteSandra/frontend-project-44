413c661c67acc3a7954f9572dd38ee7405d43a74
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-09-20 16:00:53 +0300

    add src/index.js with common part for all games

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+
+const countRound = 3;
+
+export default (description, getQuestionAndAnswer) => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+
+    console.log(`Hello, ${userName}!`);
+
+    console.log(description);
+
+    let countIteration = 0;
+    for (let i = countIteration; i < countRound; i += 1) {
+        const [question, correctAnswer] = getQuestionAndAnswer();
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('Your answer: ');
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+            countIteration = i;
+        } else {
+            console.log(`'${answer}' is wrong answer ; (.Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            break;
+        }
+    }
+    if (countIteration === 2) {
+        console.log(`Congratulations, ${userName}!`);
+    }
+};
